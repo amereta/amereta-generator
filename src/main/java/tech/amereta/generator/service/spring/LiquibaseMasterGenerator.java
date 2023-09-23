cc2b4692531cb304803774d7e7a028dcab18cd0d
Soroush <shuoros@yahoo.com>
2023-09-23 09:04:45 +0200

    IMPLEMENT[Liquibase master.xml generator]

---
+++ src/main/java/tech/amereta/generator/service/spring/LiquibaseMasterGenerator.java
@@ -0,0 +1,49 @@
+package tech.amereta.generator.service.spring;
+
+import lombok.Builder;
+import org.apache.commons.io.FileUtils;
+import tech.amereta.generator.util.soy.ISoyConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Builder
+public final class LiquibaseMasterGenerator implements ISoyConfiguration {
+
+    private List<String> changelogs;
+
+    @Override
+    public String getName() {
+        return "amereta.generator.master";
+    }
+
+    @Override
+    public File getFile() throws IOException {
+        File tempFile = File.createTempFile("master.xml", ".soy");
+        tempFile.deleteOnExit();
+
+        FileUtils.copyInputStreamToFile(
+                Objects.requireNonNull(
+                        getClass().getClassLoader().getResourceAsStream("templates/soy/master.xml.soy")
+                ), tempFile
+        );
+
+        return tempFile;
+    }
+
+    @Override
+    public Map<String, Object> getParameters() {
+        return Map.of(
+                "changelogs", changelogs
+        );
+    }
+
+    @Override
+    public Path getPath() {
+        return Path.of("src/main/resources/db/master.xml");
+    }
+}
