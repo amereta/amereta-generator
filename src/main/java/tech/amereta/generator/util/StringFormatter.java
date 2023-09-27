1111ddf5afac27515796a7809b9dfb377cd6b38d
Soroush <shuoros@yahoo.com>
2023-09-27 09:00:46 +0200

    FIX[StringFormatter.toPascalCase]

--- src/main/java/tech/amereta/generator/util/StringFormatter.java
+++ src/main/java/tech/amereta/generator/util/StringFormatter.java
@@ -1,16 +1,32 @@
 package tech.amereta.generator.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class StringFormatter {
 
+    private static final Pattern WORD_FINDER = Pattern.compile("(([A-Z]?[a-z]+)|([A-Z]))");
+
     public static String toPascalCase(String string) {
-        return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+        return findWordsInMixedCase(string)
+                .stream()
+                .map(item -> item.substring(0, 1).toUpperCase() + item.substring(1).toLowerCase())
+                .collect(Collectors.joining());
     }
 
     public static String toSnakeCase(String string) {
         return string.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
     }
 
-    public static String camelToKebab(String string) {
-        return string.replaceAll("([a-z])([A-Z])", "$1-$2").toLowerCase();
+    private static List<String> findWordsInMixedCase(String text) {
+        Matcher matcher = WORD_FINDER.matcher(text);
+        List<String> words = new ArrayList<>();
+        while (matcher.find()) {
+            words.add(matcher.group(0));
+        }
+        return words;
     }
 }
